da98b54b5c7f5e04ad4fd5407b3d8a7604e16f32
simtas <sjpayne@sfu.ca>
2015-12-08 12:29:39 -0800

    Added Word Clouds

--- WORDCLOUD/WordCloud.py
+++ WORDCLOUD/WordCloud.py
@@ -2,14 +2,18 @@ import sys
 import os
 
 
-movie_reviews_data_folder = sys.argv[1]
+movie_reviews_data_folder = "D:/CMPT/Anaconda/txt_sentoken"
 #dataset = load_files(movie_reviews_data_folder, shuffle=False)
 
 listoffiles = []
 
-f = open('AllText.txt', 'w')
-f.close
-f = open('AllText.txt', 'a')
+fneg = open('AllNeg.txt', 'w')
+fneg.close()
+fpos = open('AllPos.txt', 'w')
+fpos.close()
+
+fneg = open('AllNeg.txt', 'a')
+fpos = open('AllPos.txt', 'a')
 
 for root, dirs, files in os.walk(movie_reviews_data_folder):
     for file in files:
@@ -27,7 +31,7 @@ for i in range(0,lenlist):
         file = open(Location, 'r')
         text = file.read()
         file.close()
-        f.write(text)
+        fneg.write(text)
     else:
         if i == 1000:
             print ("HERE")
@@ -37,6 +41,6 @@ for i in range(0,lenlist):
         file = open(Location, 'r')
         text = file.read()
         file.close()
-        f.write(text)
+        fpos.write(text)
         
 f.close()
